7f2fd33b9f442ff5bd4008803cef028fc4fd33c6
Mohannad Ali <Mohannad7Ali@outlook.com>
2026-03-20 10:22:12 +0300

    feat: add dark/light theme with next-theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import CRMLayout from "@/components/layout/layout";
 import { CRMProvider } from "@/store/provider";
+import { ThemeProvider } from "@/store/themeProvider";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <CRMProvider>
-      <CRMLayout />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <CRMLayout />
+      </ThemeProvider>
     </CRMProvider>
   );
 }
